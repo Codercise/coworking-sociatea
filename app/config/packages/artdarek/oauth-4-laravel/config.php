6d9875d2a991906d016400db755bd5e925fa2781
Nick Hayden <nickhayden1992@gmail.com>
2014-07-12 12:44:32 +0930

    added oauth package

---
+++ app/config/packages/artdarek/oauth-4-laravel/config.php
@@ -0,0 +1,32 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| oAuth Config
+	|--------------------------------------------------------------------------
+	*/
+
+	/**
+	 * Storage
+	 */
+	'storage' => 'Session',
+
+	/**
+	 * Consumers
+	 */
+	'consumers' => array(
+
+		/**
+		 * Facebook
+		 */
+        'Facebook' => array(
+            'client_id'     => '1444487432481865',
+            'client_secret' => 'a535117abfd4b55cae76dda693d35208',
+            'scope'         => array(),
+        ),
+
+	)
+
+);
