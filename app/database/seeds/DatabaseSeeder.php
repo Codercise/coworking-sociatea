14bd361c7933e9784b48c4d6cf96fe3291c7c1ac
Nick Hayden <nickhayden1992@gmail.com>
2014-07-12 18:24:10 +0930

    Nick : Doesn't know what he's doing

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,21 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('VenueTableSeeder');
 	}
+
+}
+
+class VenueTableSeeder extends Seeder {
+
+    public function run()
+    {
+        //DB::table('venues')->delete();
+    		$cafeJsonOriginal = file_get_contents("app/database/seeds/data/cafe.json");
+    		var_dump($cafeJsonOriginal);
+    		$cafeJson = json_decode($cafeJsonOriginal, true);
+    		var_dump($cafeJson);
+        //Venue::create(array('email' => 'foo@bar.com'));
+    }
 
 }
